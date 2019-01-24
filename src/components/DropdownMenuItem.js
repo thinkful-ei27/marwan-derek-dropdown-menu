69bb2baf35ba467468e594c260c846b99e39d9c7
Derek Houck <derek@derekhouck.com>
2019-01-24 13:50:11 -0800

    Add styles

--- src/components/DropdownMenuItem.js
+++ src/components/DropdownMenuItem.js
@@ -2,27 +2,28 @@ import React from 'react';
 import DropDownMenu from './DropdownMenu';
 import menu from '../menu.json'
 
-export default class DropdownMenuItem  extends React.Component {
-  constructor (props) {
+export default class DropdownMenuItem extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      collapsed: false
+      collapsed: true
     }
   }
-  handleChildren(menu,id){
+  handleChildren(menu, id) {
     const children = menu.filter(menuItem => menuItem.parent === id);
-    if (children.length && !this.state.collapsed ) {
+    if (children.length && !this.state.collapsed) {
       return (<DropDownMenu menuItems={children} />);
     }
   }
-  render () {
+  handleClick(e) {
+    // Stop the click from propagating up the chain
+    e.preventDefault();
+    this.setState({ collapsed: !this.state.collapsed })
+  }
+  render() {
     return (
-      <li id={this.props.id} onClick={(e) => {
-        // Stop the click from propagating up the chain
-        e.stopPropagation();
-        this.setState({ collapsed: !this.state.collapsed })
-        }}>
-        {this.props.label}
+      <li id={this.props.id} className={!this.state.collapsed ? "active" : ""}>
+        <a href="#" onClick={(e) => this.handleClick(e)}>{this.props.label}</a>
         {this.handleChildren(menu, this.props.id)}
       </li>
     );
