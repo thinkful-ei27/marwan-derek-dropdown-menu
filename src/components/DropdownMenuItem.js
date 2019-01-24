3583d425e4a1e7534fe1dbeac9922b5eaf2c6777
Marwan Alaloosi <manoalosi@gmail.com>
2019-01-24 13:56:09 -0600

    'commited'

--- src/components/DropdownMenuItem.js
+++ src/components/DropdownMenuItem.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import DropDownMenu from './DropdownMenu';
+import menu from './menu.json'
 
 export default function DropdownMenuItem(props) {
+
+  function handleChildren(menu,id){
+    const children = [];
+   return (<DropDownMenu menuItems={children}/>)
+  }
   return (
     <li id={props.key}>
       {props.label}
+      {handleChildren(menu, props.key)}
     </li>
   );
 }
