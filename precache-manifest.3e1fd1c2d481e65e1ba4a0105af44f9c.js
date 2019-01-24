5a62eec0f50095a6e86691055628b7dade00291f
Derek Houck <derek@derekhouck.com>
2019-01-24 13:58:12 -0800

    Updates

---
+++ precache-manifest.3e1fd1c2d481e65e1ba4a0105af44f9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c45c9988df10de512ea8",
+    "url": "/marwan-derek-dropdown-menu/static/css/main.60506072.chunk.css"
+  },
+  {
+    "revision": "c45c9988df10de512ea8",
+    "url": "/marwan-derek-dropdown-menu/static/js/main.c45c9988.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/marwan-derek-dropdown-menu/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "009819b712d37355d0ef",
+    "url": "/marwan-derek-dropdown-menu/static/js/runtime~main.009819b7.js"
+  },
+  {
+    "revision": "bac9092f6f7fed47502e890670fc56a2",
+    "url": "/marwan-derek-dropdown-menu/index.html"
+  }
+];
