5fe905560edfcaf5a80c5d129ee6468c00fab9d0
Derek Houck <derek@derekhouck.com>
2019-01-24 11:15:09 -0800

    Restrict first draw to top-level menu items

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -2,8 +2,16 @@ import React from 'react';
 import DropdownMenuItem from './DropdownMenuItem';
 
 export default function DropdownMenu(props) {
+  const filterMenuItems = (menuItems, parent) => {
+    if (!parent) {
+      return menuItems.filter(menuItem => !menuItem.parent );
+    } else {
+      return menuItems;
+    }
+  };
   const createDropdownMenuItems = (menuItems) => {
-    return menuItems.map(menuItem => <DropdownMenuItem key={menuItem.id} label={menuItem.label} />);
+    const filteredMenu = filterMenuItems(menuItems, props.parent);
+    return filteredMenu.map(menuItem => <DropdownMenuItem key={menuItem.id} label={menuItem.label} parent={menuItem.parent} />);
   };
   return (
     <ul>
